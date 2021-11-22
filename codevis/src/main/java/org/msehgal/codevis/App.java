17bb4c4c1aff7b162d4fcbeffe01d46b2e7081d3
matt <matt1913@brandeis.edu>
2021-11-22 13:43:14 -0500

    ignore update

--- codevis/src/main/java/org/msehgal/codevis/App.java
+++ codevis/src/main/java/org/msehgal/codevis/App.java
@@ -3,6 +3,7 @@ package org.msehgal.codevis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,13 +11,13 @@ import org.msehgal.codevis.visualization.BasicVisualizer;
 
 import java.io.IOException;
 
-@SpringBootApplication
-@Controller
+// @SpringBootApplication
+// @Controller
 public class App {
 
 	final static String PATH = "C:/Users/matt/codevis/codevis/src/main/java/org/msehgal/codevis/util/TestSourceCode";
 	public static void main(String[] args) throws IOException {
-		SpringApplication.run(App.class, args);
+		//SpringApplication.run(App.class, args);
 		printTest();
 	}
 
